d765d97fd70fedd200cb4b6c15dd81eff4e3b70a
dalture <300195d@gmail.com>
2025-12-07 15:13:10 +0300

    language function

--- src/nlp/language.py
+++ src/nlp/language.py
@@ -1,2 +1,36 @@
-def detect_language():
-    pass
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+http_proxy = os.getenv("HTTP_PROXY")
+https_proxy = os.getenv("HTTPS_PROXY")
+os.environ['HTTP_PROXY'] = http_proxy
+os.environ['HTTPS_PROXY'] = https_proxy
+
+model_ckpt = "papluca/xlm-roberta-base-language-detection"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt, force_download=True)
+model = AutoModelForSequenceClassification.from_pretrained(model_ckpt)
+model.eval()
+
+def detect_language(text):
+    inputs = tokenizer(
+        text,
+        padding=True,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+
+    probs = torch.softmax(logits, dim=-1)
+    prob, idx = torch.max(probs, dim=1)
+    
+    lang = model.config.id2label[idx.item()]
+    
+    if lang not in ['en', 'ru']:
+        return 'unidentified'
+    else:
+        return lang
